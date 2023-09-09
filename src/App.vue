b9d34e622e0044ae2b73680cb92f73ca8d251ae2
AlbertXZH <738822655@qq.com>
2023-09-09 18:37:26 +0800

    router设置完成

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
-    <FirstPage msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
-
 <script>
 import FirstPage from './components/FirstPage.vue'
 
@@ -20,8 +19,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="hello">
-    let we begin
+    <el-row>
+      <el-col :span="24">
+        <div class="header">
+          111
+        </div>
+      </el-col>
+    </el-row>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'FirstPage',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      radio: '1'
+    };
   }
 }
 </script>
@@ -18,15 +30,22 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
+.header{
+  background-color: rgba(0, 0, 0, .5);
+}
+
 </style>
