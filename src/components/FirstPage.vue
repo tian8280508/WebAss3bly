a4fdf1cc2edeccd088431e3a79d9d0e9c974d376
AlbertXZH <738822655@qq.com>
2023-09-10 08:35:08 +0800

    比较失败的echart

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -76,7 +76,7 @@ export default { //这个是一个Vue对象
     }
   },
   methods: {
-    drawChartFirst() {
+    getChartData() {
       var chartData = {
         name: 'flare',
         children: [
@@ -195,6 +195,129 @@ export default { //这个是一个Vue对象
           }
         ]
       };
+      this.chartData = chartData
+    },
+
+    drawChartFirst() {
+      var chartData = {
+        name: 'flare',
+        children: [
+          {
+            name: 'data',
+            children: [
+              {
+                name: 'converters',
+                children: [
+                  { name: 'Converters'},
+                  { name: 'DelimitedTextConverter', value: 4294 }
+                ]
+              },
+              {
+                name: 'DataUtil',
+                value: 3322
+              }
+            ]
+          },
+          {
+            name: 'display',
+            children: [
+              { name: 'DirtySprite', value: 8833 },
+              { name: 'LineSprite', value: 1732 },
+              { name: 'RectSprite', value: 3623 }
+            ]
+          },
+          {
+            name: 'flex',
+            children: [{ name: 'FlareVis', value: 4116 }]
+          },
+          {
+            name: 'query',
+            children: [
+              { name: 'AggregateExpression', value: 1616 },
+              { name: 'And', value: 1027 },
+              { name: 'Arithmetic', value: 3891 },
+              { name: 'Average', value: 891 },
+              { name: 'BinaryExpression', value: 2893 },
+              { name: 'Comparison', value: 5103 },
+              { name: 'CompositeExpression', value: 3677 },
+              { name: 'Count', value: 781 },
+              { name: 'DateUtil', value: 4141 },
+              { name: 'Distinct', value: 933 },
+              { name: 'Expression', value: 5130 },
+              { name: 'ExpressionIterator', value: 3617 },
+              { name: 'Fn', value: 3240 },
+              { name: 'If', value: 2732 },
+              { name: 'IsA', value: 2039 },
+              { name: 'Literal', value: 1214 },
+              { name: 'Match', value: 3748 },
+              { name: 'Maximum', value: 843 },
+              {
+                name: 'methods',
+                children: [
+                  { name: 'add', value: 593 },
+                  { name: 'and', value: 330 },
+                  { name: 'average', value: 287 },
+                  { name: 'count', value: 277 },
+                  { name: 'distinct', value: 292 },
+                  { name: 'div', value: 595 },
+                  { name: 'eq', value: 594 },
+                  { name: 'fn', value: 460 },
+                  { name: 'gt', value: 603 },
+                  { name: 'gte', value: 625 },
+                  { name: 'iff', value: 748 },
+                  { name: 'isa', value: 461 },
+                  { name: 'lt', value: 597 },
+                  { name: 'lte', value: 619 },
+                  { name: 'max', value: 283 },
+                  { name: 'min', value: 283 },
+                  { name: 'mod', value: 591 },
+                  { name: 'mul', value: 603 },
+                  { name: 'neq', value: 599 },
+                  { name: 'not', value: 386 },
+                  { name: 'or', value: 323 },
+                  { name: 'orderby', value: 307 },
+                  { name: 'range', value: 772 },
+                  { name: 'select', value: 296 },
+                  { name: 'stddev', value: 363 },
+                  { name: 'sub', value: 600 },
+                  { name: 'sum', value: 280 },
+                  { name: 'update', value: 307 },
+                  { name: 'variance', value: 335 },
+                  { name: 'where', value: 299 },
+                  { name: 'xor', value: 354 },
+                  { name: 'x_x', value: 264 }
+                ],
+                value:"test",
+              },
+              { name: 'Minimum', value: 843 },
+              { name: 'Not', value: 1554 },
+              { name: 'Or', value: 970 },
+              { name: 'Query', value: 13896 },
+              { name: 'Range', value: 1594 },
+              { name: 'StringUtil', value: 4130 },
+              { name: 'Sum', value: 791 },
+              { name: 'Variable', value: 1124 },
+              { name: 'Variance', value: 1876 },
+              { name: 'Xor', value: 1101 }
+            ]
+          },
+          {
+            name: 'scale',
+            children: [
+              { name: 'IScaleMap', value: 2105 },
+              { name: 'LinearScale', value: 1316 },
+              { name: 'LogScale', value: 3151 },
+              { name: 'OrdinalScale', value: 3770 },
+              { name: 'QuantileScale', value: 2435 },
+              { name: 'QuantitativeScale', value: 4839 },
+              { name: 'RootScale', value: 1756 },
+              { name: 'Scale', value: 4268 },
+              { name: 'ScaleType', value: 1821 },
+              { name: 'TimeScale', value: 5833 }
+            ]
+          }
+        ]
+      };
       this.chartOption.series[0].data = [chartData];
       let myChart = echarts.init(document.getElementById('myChart'))
       // 绘制图表
@@ -203,23 +326,80 @@ export default { //这个是一个Vue对象
       );
     },
     drawChart() {
-      let myChart = echarts.init(document.getElementById('myChart'))
+      let myChart = echarts.init(document.getElementById('myChart'), null)
       // 绘制图表
+      console.log(JSON.stringify(this.chartOption.series[0].data));
+      this.chartOption.series[0].data = JSON.parse(JSON.stringify(this.chartData));
+      // myChart.clear();
       myChart.setOption(
-        this.chartOption
-      );
+        {
+          tooltip: {
+            trigger: 'item',
+            triggerOn: 'mousemove'
+          },
+          series: [
+            {
+              type: 'tree',
+              id: 0,
+              name: 'tree1',
+              data: [this.chartData],
+              // top: '10%',
+              // left: '8%',
+              // bottom: '22%',
+              // right: '20%',
+              symbolSize: 8,
+              edgeShape: 'polyline',
+              edgeForkPosition: '3%',
+              initialTreeDepth: 3,
+              lineStyle: {
+                width: 2
+              },
+              label: {
+                backgroundColor: '#fff',
+                position: 'left',
+                verticalAlign: 'middle',
+                align: 'right'
+              },
+              leaves: {
+                label: {
+                  position: 'right',
+                  verticalAlign: 'middle',
+                  align: 'left'
+                }
+              },
+              emphasis: {
+                focus: 'descendant'
+              },
+              expandAndCollapse: true,
+              animationDuration: 550,
+              animationDurationUpdate: 750
+            }
+          ]
+        },
+      )
+        // myChart.setOption(this.chartOption)
 
     }
   },
-  computed: {
-    chartDataActivate() {
-      console.log("activate");
-      return this.chartOption.series[0].data = [this.chartData];
+  watch: {
+    chartData(newVal, oldVal) {
+      if (oldVal != newVal) {
+        this.drawChart()
+      }
     }
   },
+  // computed: {
+  //   chartDataActivate() {
+  //     console.log("activate");
+  //     return this.chartOption.series[0].data = [this.chartData];
+  //   }
+  // },
   mounted() {
-      this.drawChartFirst();
-    },
+    this.getChartData()// 加载数据 触发watch 触发其中drawChart函数
+    // this.drawChartFirst();
+  },
+  created() {
+  }
 }
 </script>
 
@@ -249,8 +429,9 @@ a {
 }
 
 #myChart {
-  width: 500px;
-  height: 500px;
-  background-color: #42b983;
+  /* width: 500px;可以没有这个 */
+  height: 550px;
+  /* 不可以100% */
+  /* background-color: #42b983; */
 }
 </style>
